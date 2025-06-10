45cf652d1d2b52b19b55c7cbf418ece5343f246e
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-10 22:22:44 +0500

    db test

--- utils/db/database.py
+++ utils/db/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import (create_engine, Column, Integer,
                         String, BigInteger)
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 from data.config import PG_USER, PG_PASS, PG_HOST, PG_PORT, PG_DB
 
 engine = create_engine(f'postgresql+psycopg2://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}')
 
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 class User(Base):
@@ -14,3 +16,13 @@ class User(Base):
     chat_id = Column(BigInteger, unique=True)
     fullname = Column(String(50), nullable=False)
     phone = Column(String(12))
+
+
+if __name__ == '__main__':
+    user = User(chat_id=1836679375, fullname='Asadbek Solijonov', phone='998911779116')
+    session.add(user)
+    # user = session.query(User).filter(1836679375 == User.chat_id).first()
+    # if user:
+        # user.fullname = 'Solijonov Asadbek'
+        # session.delete(user)
+    session.commit()

fbf56952319e43eae75c84d9d8b901ef4746dff8
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-06-12 21:53:39 +0500

    db lang + middleware

--- middlewares/db_i18n.py
+++ middlewares/db_i18n.py
@@ -3,7 +3,12 @@ from typing import Dict, Any
 from aiogram.types import TelegramObject
 from aiogram.utils.i18n import I18nMiddleware
 
+from utils.db.database import session
+from utils.db_helper import get_user_db_lang
+
 
 class DatabaseI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
-        return self.i18n.default_locale
+        user = data.get('event_from_user')
+        user_lang = await get_user_db_lang(session, user.id)
+        return user_lang or self.i18n.default_locale
